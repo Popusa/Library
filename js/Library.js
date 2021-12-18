88b150d1161cce73113ea8f692a5c29f6c2b924b
popusa <danielmaged.3@gmail.com>
2021-12-18 23:10:44 +0200

    added basic boilerplate and code for Book Object Constructor

---
+++ js/Library.js
@@ -0,0 +1,10 @@
+function Book(Title,Author,NumOfPages,DateOfPublication,Read){
+    this.Title = Title;
+    this.Author = Author;
+    this.NumOfPages = NumOfPages;
+    this.DateOfPublication = DateOfPublication;
+    this.Read = Read;
+}
+Book.prototype.PrintDetails = function(){
+    return "Title: " + this.Title + " Author: " + this.Author + " Page Count:" + this.NumOfPages + " Date Of Publication: " + this.DateOfPublication + "This book's read status is: " + read;
+}
