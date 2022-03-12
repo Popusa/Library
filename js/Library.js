7bfb3b714e38fd519e402ad6dc71b9d0f5a464b3
popusa <danielmaged.3@gmail.com>
2022-03-12 23:59:36 +0200

    renamed all vars and functions to have consistent naming convention

--- js/Library.js
+++ js/Library.js
@@ -12,33 +12,33 @@ let edited_book_idx;
 let title_error_msg = "Title is too long.",author_error_Msg = "Author name is too long.",num_of_Pages_error_msg = "Number of pages is either too little or not even a number.",no_errors = "No errors.";
 let books_arr = [];
 let book_count = 0;
-function Book(Title,Author,NumOfPages,Read){
-    this.Title = Title;
-    this.Author = Author;
-    this.NumOfPages = NumOfPages;
-    this.Read = Read;
+function book(title,author,num_of_pages,read){
+    this.title = title;
+    this.author = author;
+    this.num_of_pages = num_of_pages;
+    this.read = read;
 }
-Book.prototype.PrintDetails = function(){
-    return "Book Number: " + book_count + ", Title: " + this.Title + " Author: " + this.Author + " Page Count: " + this.NumOfPages + " This book's read status is: " + this.Read;
+book.prototype.print_details = function(){
+    return "Book Number: " + book_count + ", Title: " + this.title + " Author: " + this.author + " Page Count: " + this.num_of_pages + " This book's read status is: " + this.read;
 }
-function StoreBook(BookObject){
-    books_arr.push(BookObject);
+function store_book(book_object){
+    books_arr.push(book_object);
     localStorage.setItem('books_arr', JSON.stringify(books_arr));
     book_count++;
     localStorage.setItem('book_count',book_count);
 }
-function EditBook(){
-    books_arr[edited_book_idx].Title = edit_book_form.elements[0].value;
-    books_arr[edited_book_idx].Author = edit_book_form.elements[1].value;
-    books_arr[edited_book_idx].NumOfPages = edit_book_form.elements[2].value;
+function edit_book(){
+    books_arr[edited_book_idx].title = edit_book_form.elements[0].value;
+    books_arr[edited_book_idx].author = edit_book_form.elements[1].value;
+    books_arr[edited_book_idx].num_of_pages = edit_book_form.elements[2].value;
     if (edit_book_form.elements[3].checked == true)
         books_arr[edited_book_idx].Read = true;
     else
         books_arr[edited_book_idx].Read = false;
         localStorage.setItem('books_arr', JSON.stringify(books_arr));
-        DisplayAllBooks();
+        display_all_books();
 }
-function CreateBookDiv(BookObj){
+function create_book_div(book_obj){
     const book_div = document.createElement('div');
     const title_div = document.createElement('div');
     const author_div = document.createElement('div');
@@ -47,22 +47,23 @@ function CreateBookDiv(BookObj){
     const edit_book_button = document.createElement('button');
     const read_status_button = document.createElement('button');
     book_div.classList.add('book');
-    book_div.setAttribute('id', books_arr.indexOf(BookObj));
-    title_div.innerText = BookObj.Title;
+    book_div.setAttribute('id', books_arr.indexOf(book_obj));
+    title_div.innerText = book_obj.title;
     title_div.classList.add('title_div');
     book_div.appendChild(title_div);
-    author_div.innerText = BookObj.Author;
+    author_div.innerText = book_obj.author;
     author_div.classList.add('author_div');
     book_div.appendChild(author_div);
-    nop_div.innerText = BookObj.NumOfPages + " pgs";
+    nop_div.innerText = book_obj.num_of_pages + " pgs";
     nop_div.classList.add('nop_div');
     book_div.appendChild(nop_div);
     read_status_button.classList.add('read_status_button');    
     book_div.appendChild(read_status_button);
-    if(BookObj.Read == false) {
+    if(book_obj.Read == false) {
         read_status_button.innerText = 'Not Read';
         read_status_button.style.backgroundColor = 'red';
-    }else{
+    }
+    else{
         read_status_button.innerText = 'Read';
         read_status_button.style.backgroundColor = 'green';
     }
@@ -74,15 +75,15 @@ function CreateBookDiv(BookObj){
     book_div.appendChild(remove_book_button);
     all_books.appendChild(book_div);
     remove_book_button.addEventListener('click',function(){
-        books_arr.splice(books_arr.indexOf(BookObj),1);
+        books_arr.splice(books_arr.indexOf(book_obj),1);
         localStorage.setItem('books_arr', JSON.stringify(books_arr));
-        DisplayAllBooks();
+        display_all_books();
         book_count--;
         localStorage.setItem('book_count',book_count);
     });
     read_status_button.addEventListener('click',function(){
-        BookObj.Read = !BookObj.Read;
-        if(BookObj.Read == false) {
+        book_obj.Read = !book_obj.Read;
+        if(book_obj.Read == false) {
             read_status_button.innerText = 'Not Read';
             read_status_button.style.backgroundColor = 'red';
         }
@@ -91,75 +92,75 @@ function CreateBookDiv(BookObj){
             read_status_button.style.backgroundColor = 'green';
         }
         localStorage.setItem('books_arr', JSON.stringify(books_arr));
-        DisplayAllBooks();
+        display_all_books();
     });
     edit_book_button.addEventListener('click',function(){
         if (!editing_book){
             edit_book_popup.style.display = "block";
             editing_book = true;
-            edit_book_form.elements[0].value = BookObj.Title;
-            edit_book_form.elements[1].value = BookObj.Author;
-            edit_book_form.elements[2].value = BookObj.NumOfPages; 
-            edited_book_idx = books_arr.indexOf(BookObj);
+            edit_book_form.elements[0].value = book_obj.title;
+            edit_book_form.elements[1].value = book_obj.author;
+            edit_book_form.elements[2].value = book_obj.num_of_pages; 
+            edited_book_idx = books_arr.indexOf(book_obj);
         }
         else
             return;
     });
 }
-function ValidateForm(){
-    let Temp_Title;
-    let Temp_Author;
-    let Temp_NumOfPages;      
+function validate_form(){
+    let temp_title;
+    let temp_author;
+    let temp_num_of_pages;      
     if (editing_book){
-        Temp_Title = edit_book_form.elements[0].value;
-        Temp_Author = edit_book_form.elements[1].value;
-        Temp_NumOfPages = edit_book_form.elements[2].value;        
+        temp_title = edit_book_form.elements[0].value;
+        temp_author = edit_book_form.elements[1].value;
+        temp_num_of_pages = edit_book_form.elements[2].value;        
     }
     else{
-        Temp_Title = add_book_form.elements[0].value;
-        Temp_Author = add_book_form.elements[1].value;
-        Temp_NumOfPages = add_book_form.elements[2].value;
+        temp_title = add_book_form.elements[0].value;
+        temp_author = add_book_form.elements[1].value;
+        temp_num_of_pages = add_book_form.elements[2].value;
     }
-    if (Temp_Title.length > 20)
+    if (temp_title.length > 20)
         return title_error_msg;
-    else if (Temp_Author.length > 20)
+    else if (temp_author.length > 20)
         return author_error_Msg;
-    else if (Temp_NumOfPages < 20 || isNaN(Temp_NumOfPages))
+    else if (temp_num_of_pages < 20 || isNaN(temp_num_of_pages))
         return num_of_Pages_error_msg;
     else
         return no_errors;
 }
-function AddNewbook(){ 
-    console.log("function was called");
-    if (book_count >= 39){
-        alert("limit reached.");
-        return;
-    }
-    let NewBook = new Book();
-    NewBook.Title = add_book_form.elements[0].value;
-    NewBook.Author = add_book_form.elements[1].value;
-    NewBook.NumOfPages = add_book_form.elements[2].value;
+function add_new_book(){ 
+    //console.log("function was called");
+    // if (book_count >= 39){
+    //     alert("limit reached.");
+    //     return;
+    // }
+    let new_book = new book();
+    new_book.title = add_book_form.elements[0].value;
+    new_book.author = add_book_form.elements[1].value;
+    new_book.num_of_pages = add_book_form.elements[2].value;
     if (document.getElementById('read_status_yes').checked == true)
-        NewBook.Read = true;
+        new_book.Read = true;
     else
-        NewBook.Read = false;
-    StoreBook(NewBook);
-    CreateBookDiv(NewBook);
-    console.log(NewBook.PrintDetails());
+        new_book.Read = false;
+    store_book(new_book);
+    create_book_div(new_book);
+    console.log(new_book.print_details());
     // console.log("function reached end.");
 }
-function DisplayAllBooks(){
+function display_all_books(){
     const books = document.querySelectorAll('.book');
     books.forEach(book => all_books.removeChild(book));
     for (let i = 0; i < books_arr.length; i++)
-        CreateBookDiv(books_arr[i]);
+        create_book_div(books_arr[i]);
 }
-function DisplayBookForm(){
+function display_book_form(){
     add_book_button.innerText = "Cancel";
     add_book_popup.style.display = "block";
     creating_book = true;
 }
-function HideBookForm(){
+function hide_book_form(){
     if (editing_book){
         edit_book_popup.style.display = "none";
         editing_book = false;
@@ -172,27 +173,27 @@ function HideBookForm(){
 }
 add_book_form && add_book_form.addEventListener('submit',function(e) {
     e.preventDefault();
-    let Error_Msg = ValidateForm();
-    if (Error_Msg != no_errors){
-        alert(Error_Msg);
+    let error_msg = validate_form();
+    if (error_msg != no_errors){
+        alert(error_msg);
         return;
     }
     else {
-        HideBookForm();
-        AddNewbook();
+        hide_book_form();
+        add_new_book();
         add_book_form.reset();
     }
 });
 edit_book_form && edit_book_form.addEventListener('submit',function(e){
     e.preventDefault();
-    let Error_Msg = ValidateForm();
-    if (Error_Msg != no_errors){
-        alert(Error_Msg);
+    let error_msg = validate_form();
+    if (error_msg != no_errors){
+        alert(error_msg);
         return;
     }
     else {
-        HideBookForm();
-        EditBook();
+        hide_book_form();
+        edit_book();
         edit_book_form.reset();
     }
 });
@@ -203,26 +204,26 @@ add_book_button.addEventListener('click',function(){
     }
     else{
     if (creating_book){
-        HideBookForm();
+        hide_book_form();
     }
     else
-        DisplayBookForm();
+        display_book_form();
 }
 });
 close_edit_book_form_button.addEventListener('click',function(){
     edit_book_popup.style.display = "none";
     editing_book = false;
 });
-function LoadStoredDetails(){
+function load_stored_details(){
     if(localStorage.getItem("books_arr") === null) {
         books_arr = [];
         book_count = 0;
     }
     else {
-        let BookObjects = localStorage.getItem('books_arr');
-        BookObjects = JSON.parse(BookObjects);
-        books_arr = BookObjects;
-        DisplayAllBooks();
+        let book_objects = localStorage.getItem('books_arr');
+        book_objects = JSON.parse(book_objects);
+        books_arr = book_objects;
+        display_all_books();
         if (book_count === NaN)
             book_count = books_arr.length;
         else
@@ -233,8 +234,8 @@ add_book_popup.style.display = "none";
 edit_book_popup.style.display = "none";
 //testing purposes
 // for(let i = 0; i < 45; i++){
-// const Test = new Book("The Power of Habit","Charles Duhigg","371",true);
-// StoreBook(Test);
-// CreateBookDiv(Test);
+// const test = new book("The Power of Habit","Charles Duhigg","371",true);
+// store_book(test);
+// create_book_div(Test);
 // }
-LoadStoredDetails();
+load_stored_details();
